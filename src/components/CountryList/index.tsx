7b35445830088f96dcb43b06b721e8c5d227fe67
Henry Tottle <henrywilliamtottle@gmail.com>
2024-10-10 14:48:33 +0100

    take three

--- src/components/CountryList/index.tsx
+++ src/components/CountryList/index.tsx
@@ -1,56 +1,60 @@
-import {JSX, useState, useEffect} from "react";
+import { JSX, useState, useEffect } from "react";
 
+interface Country {
+    name: {
+        common: string;
+    };
+    capital: string[];
+    languages: Record<string, string>;
+    currencies: Record<string, { name: string }>;
+}
 
+interface EachCountryProps {
+    continent: string;
+}
 
-function EachCountry({continent}: any): JSX.Element {
-    const [countries, setCountries] = useState([])
-
-
+function EachCountry({ continent }: EachCountryProps): JSX.Element {
+    const [countries, setCountries] = useState<Country[]>([]);
 
     useEffect(() => {
-        const fetchCountries = async():Promise<void> => {
-            const response = await fetch('https://restcountries.com/v3.1/region/'+continent+'?fields=name,capital,languages,currencies')
-            const countryArray = await response.json()
+        const fetchCountries = async (): Promise<void> => {
+            const response = await fetch(
+                `https://restcountries.com/v3.1/region/${continent}?fields=name,capital,languages,currencies`
+            );
+            const countryArray = await response.json();
+            setCountries(countryArray);
+        };
+        fetchCountries();
+    }, [continent]);
 
-            setCountries(countryArray)
-        }
-        fetchCountries()
-    }, [continent])
-
-    let image = ''
-if (continent === 'north%20america') {
-    image = 'northAmerica'
-} else if (continent === 'south%20america') {
-    image = 'southAmerica'
-} else {
-    image = continent
-}
+    let image = '';
+    if (continent === 'north%20america') {
+        image = 'northAmerica';
+    } else if (continent === 'south%20america') {
+        image = 'southAmerica';
+    } else {
+        image = continent;
+    }
 
     return (
         <>
-            <img className='pl-32' src={image + '.jpeg'}/>
-            {countries.map((country: any,index:number) =>
-            {
-
-                return (
-                    <div key={index} className='m-1 text-center border-2 rounded-3xl shadow-2xl shadow-cyan-950 py-2 hover:bg-blue-500'>
-                        <p className='font-black text-2xl'>{country.name.common}</p>
-
-                        <p>Capital: {country.capital.join(', ')}</p>
-
-                        <p>Language: {Object.values(country.languages)[0]}</p>
-
-                        {Object.values(country.languages)[1] && <p>Other Language: {Object.values(country.languages)[1]}</p>}
-
-                        <p>Currency: {Object.keys(country.currencies)[0]}</p>
-                    </div>
-                )
-            })
-            }
+            <img className='pl-32' src={`${image}.jpeg`} alt={`${continent} image`} />
+            {countries.map((country, index) => (
+                <div
+                    key={index}
+                    className='m-1 text-center border-2 rounded-3xl shadow-2xl shadow-cyan-950 py-2 hover:bg-blue-500'
+                >
+                    <p className='font-black text-2xl'>{country.name.common}</p>
+                    <p>Capital: {country.capital.join(', ')}</p>
+                    <p>Language: {Object.values(country.languages)[0]}</p>
+                    {Object.values(country.languages)[1] && (
+                        <p>Other Language: {Object.values(country.languages)[1]}</p>
+                    )}
+                    <p>Currency: {Object.keys(country.currencies)[0]}</p>
+                </div>
+            ))}
         </>
-    )
-
-
+    );
 }
 
-export default EachCountry
+export default EachCountry;
